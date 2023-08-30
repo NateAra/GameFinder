214c4a8ea75a616eeca992e0f09b229b1761439d
NateAra <nate.arafayne@appliedtechnology.se>
2023-08-30 10:23:22 +0200

    Added alert using State variable

--- game-finder/src/App.tsx
+++ game-finder/src/App.tsx
@@ -1,11 +1,17 @@
+import { useState } from "react";
+import { Alert } from "./components/Alert";
 import { Button } from "./components/Button";
 
 const App = () => { 
 
+  const [alertVisible, setAlertVisibility] = useState(false)
+
   return (
     <div>
-      <Button color="info" handleClick={() => console.log('Clicked')}>
-        My Button
+      {alertVisible && <Alert onClose={() => setAlertVisibility(false)}>My Alert</Alert>}
+
+      <Button color="info" handleClick={() => setAlertVisibility(true)}>
+        Alert
       </Button>
     </div>
   );
---
+++ game-finder/src/components/Alert.tsx
@@ -0,0 +1,22 @@
+interface AlertProp {
+  children: string;
+  onClose: () => void;
+}
+
+export const Alert = ({ children, onClose }: AlertProp) => {
+  return (
+    <div
+      className="alert alert-warning alert-dismissible fade show"
+      role="alert"
+    >
+      {children}
+      <button
+        type="button"
+        className="btn-close"
+        data-bs-dismiss="alert"
+        aria-label="Close"
+        onClick={onClose}
+      ></button>
+    </div>
+  );
+};
