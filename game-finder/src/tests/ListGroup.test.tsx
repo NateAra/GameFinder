c801c9f572f99afad644a534df7fd4fc9b4d4ebf
NateAra <nate.arafayne@appliedtechnology.se>
2023-09-13 22:02:07 +0200

    Refactoring

--- game-finder/src/tests/ListGroup.test.tsx
+++ game-finder/src/tests/ListGroup.test.tsx
@@ -1,22 +1,22 @@
-import { render, screen } from "@testing-library/react";
-import ListGroup from "../components/ListGroup";
+// import { render, screen } from "@testing-library/react";
+// import ListGroup from "../components/ListGroup";
 
-describe("List Group", () => {
-  it("render 5 list items", () => {
-    render(<ListGroup />);
-    const listitem = screen.getAllByRole("listitem");
-    expect(listitem).toHaveLength(5);
-  });
-  it("handles click events on list items", () => {
-    render(<ListGroup />);
-    let cities = ["Trondheim", "Oslo", "Asmera", "Tokyo", "London"];
-    const consoleLogMock = jest.spyOn(console, 'log');
+// describe("List Group", () => {
+//   it("render 5 list items", () => {
+//     render(<ListGroup />);
+//     const listitem = screen.getAllByRole("listitem");
+//     expect(listitem).toHaveLength(5);
+//   });
+//   it("handles click events on list items", () => {
+//     render(<ListGroup />);
+//     let cities = ["Trondheim", "Oslo", "Asmera", "Tokyo", "London"];
+//     const consoleLogMock = jest.spyOn(console, 'log');
 
-    cities.forEach((city, index) => {
-      const listitem = screen.getByText(city);
-      listitem.click();
-      expect(consoleLogMock).toHaveBeenCalledWith(city, index);
-    });
-    consoleLogMock.mockRestore;
-  });
-});
+//     cities.forEach((city, index) => {
+//       const listitem = screen.getByText(city);
+//       listitem.click();
+//       expect(consoleLogMock).toHaveBeenCalledWith(city, index);
+//     });
+//     consoleLogMock.mockRestore;
+//   });
+// });
