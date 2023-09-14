afb8b328029a0b0dc71889c219ced2c65c524db9
NateAra <nate.arafayne@appliedtechnology.se>
2023-09-14 21:09:00 +0200

    Refactoring

--- game-finder/src/App.tsx
+++ game-finder/src/App.tsx
@@ -1,22 +1,9 @@
 import { useState } from "react";
-import { Alert } from "./components/Alert";
-import { Button } from "./components/Button";
-import { Like } from "./components/Like";
 
 const App = () => {
-  const [alertVisible, setAlertVisibility] = useState(false);
 
   return (
     <div>
-      {alertVisible && (
-        <Alert onClose={() => setAlertVisibility(false)}>My Alert</Alert>
-      )}
-
-      <Button color="info" handleClick={() => setAlertVisibility(true)}>
-        Alert
-      </Button>
-
-      <Like heartClick={() => console.log('Clicked')}/>
     </div>
   );
 };
