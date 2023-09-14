7ce894ab6529b77a77d3b0a7ab849d7bf6774eeb
NateAra <nate.arafayne@appliedtechnology.se>
2023-09-14 22:14:10 +0200

    Testing my git repo

---
+++ game-finder/src/components/ListGroup.tsx
@@ -0,0 +1,14 @@
+const ListGroup = () => {
+  return (
+    <ul className="list-group">
+      <li className="list-group-item">Item 1</li>
+      <li className="list-group-item">Item 2</li>
+      <li className="list-group-item">Item 3</li>
+      <li className="list-group-item">Item 4</li>
+    </ul>
+  );
+};
+
+export default ListGroup;
+
+// this is a test 
