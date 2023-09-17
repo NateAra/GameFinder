c352aaa1c6069a528cdfb18f15f1707a848cbeec
NateAra <nate.arafayne@appliedtechnology.se>
2023-09-17 17:50:30 +0200

    Conditional Rendering items

--- game-finder/src/components/ListGroup.tsx
+++ game-finder/src/components/ListGroup.tsx
@@ -2,13 +2,21 @@ const ListGroup = () => {
   const itemList = ["item1", "item2", "item3", "item4"];
 
   return (
-    <ul className="list-group">
-      {itemList.map((item) => (
-        <li key={item} className="list-group-item">{item}</li>
-      ))}
-    </ul>
+    <>
+      <h1>Lists</h1>
+      {itemList.length === 0 ? (
+        <p>No Items</p>
+      ) : (
+        <ul className="list-group">
+          {itemList.map((item) => (
+            <li key={item} className="list-group-item">
+              {item}
+            </li>
+          ))}
+        </ul>
+      )}
+    </>
   );
 };
 
 export default ListGroup;
-
