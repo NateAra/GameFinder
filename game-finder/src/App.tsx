fa7eaf0da2506953762e9cb8460e0da6cbdcdc7a
NateAra <nate.arafayne@appliedtechnology.se>
2023-09-21 09:02:44 +0200

    Experimenting with function props

--- game-finder/src/App.tsx
+++ game-finder/src/App.tsx
@@ -8,7 +8,7 @@ const App = () => {
 
   return (
     <div>
-      <Button onClick={handleOnClick}>
+      <Button onClick={handleOnClick} color="">
         My Button
       </Button>
     </div>
--- game-finder/src/components/Button.tsx
+++ game-finder/src/components/Button.tsx
@@ -2,12 +2,13 @@ import React from 'react'
 
 interface ButtonProp {
     children: string;
+    color: string;
     onClick: () => void;
 }
 
-const Button = ( { children, onClick}: ButtonProp) => {
+const Button = ( { children, color = "Info", onClick}: ButtonProp) => {
   return (
-    <div className='btn btn-primary'>{children}</div>
+    <div className={'btn btn-' + color} onClick={onClick}>{children}</div>
   )
 }
 
