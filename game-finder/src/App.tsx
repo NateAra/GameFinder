947944e1eb69b268f5f1e375f07cb1cb510e1339
NateAra <nate.arafayne@appliedtechnology.se>
2023-09-23 13:16:01 +0200

    installed chakra ui

--- game-finder/src/App.tsx
+++ game-finder/src/App.tsx
@@ -1,23 +1,8 @@
-import { useState } from "react";
 
 const App = () => {
-  const [tag, setTag] = useState(['Happy', ' Sad']);
-
-  const handleClick = () => {
-    // setTag([...tag, ' Cheerful']);
-
-    setTag(tag.filter(tag => tag !== ' Sad'));
-
-    //Update
-    // setTag(tag.map(tag => tag === 'Happy' ? 'Happiness' : tag));
-  }
 
   return (
     <div>
-      <button className="btn btn-info" onClick={handleClick}>
-        Click ME
-      </button>
-      {tag}
     </div>
   );
 };
