9e5771810ee83da1de9838bbfdea3e932163ea91
NateAra <nate.arafayne@appliedtechnology.se>
2023-09-24 13:02:42 +0200

    Added theme.ts to the root, with some chakra config

--- game-finder/src/main.tsx
+++ game-finder/src/main.tsx
@@ -1,11 +1,13 @@
 import React from "react";
-import { ChakraProvider } from "@chakra-ui/react";
+import { ChakraProvider, ColorModeScript } from "@chakra-ui/react";
 import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
+import theme from "./theme.ts";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
-    <ChakraProvider>
+    <ChakraProvider theme={theme} >
+    <ColorModeScript initialColorMode={theme.config.initialColorMode}/>
       <App />
     </ChakraProvider>
   </React.StrictMode>
