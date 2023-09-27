64b4bc89a5ae0e573a3a0c919913594c3b6286fb
NateAra <nate.arafayne@appliedtechnology.se>
2023-09-27 21:08:03 +0200

    Refactored code

--- game-finder/src/hooks/useGames.ts
+++ game-finder/src/hooks/useGames.ts
@@ -2,34 +2,33 @@ import axios from "axios";
 import { useEffect, useState } from "react";
 
 interface Game {
-    id: number;
-    name: string;
-  }
-  
-  interface FetchGamesResponse {
-    count: number;
-    results: Game[];
-  }
-
-const useGames = () => {
-
-    const [games, setGames] = useState<Game[]>([]);
-    const [error, setError] = useState("");
-  
-    useEffect(() => {
-        const controller = new AbortController();
-
-      axios
-        .get<FetchGamesResponse>("http://localhost:3001", {signal: controller.signal})
-        .then((response) => setGames(response.data.results))
-        .catch((error) => setError(error.message));
-
-        return () => controller.abort();
-    }, []);
-
-  return (
-    {games, error}
-  )
+  id: number;
+  name: string;
 }
 
-export default useGames
+interface FetchGamesResponse {
+  count: number;
+  results: Game[];
+}
+
+const useGames = () => {
+  const [games, setGames] = useState<Game[]>([]);
+  const [error, setError] = useState("");
+
+  useEffect(() => {
+    const controller = new AbortController();
+
+    axios
+      .get<FetchGamesResponse>("http://localhost:3001", {
+        signal: controller.signal,
+      })
+      .then((response) => setGames(response.data.results))
+      .catch((error) => setError(error.message));
+
+    return () => controller.abort();
+  }, []);
+
+  return { games, error };
+};
+
+export default useGames;
