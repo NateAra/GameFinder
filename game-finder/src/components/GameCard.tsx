fa94825cd31cc54200f0a4467c9c9cbc78d2b95e
NateAra <nate.arafayne@appliedtechnology.se>
2023-09-27 23:09:04 +0200

    Added platform icon properties from rawg

--- game-finder/src/components/GameCard.tsx
+++ game-finder/src/components/GameCard.tsx
@@ -1,19 +1,23 @@
-import { Game } from '../hooks/useGames'
-import { Card, CardBody, Heading, Image } from '@chakra-ui/react'
+import { Game } from "../hooks/useGames";
+import { Card, CardBody, Heading, Image } from "@chakra-ui/react";
+import PlatformIconList from "./PlatformIconList";
 
 interface GameProp {
-    game: Game
+  game: Game;
 }
 
-const GameCard = ( {game}: GameProp ) => {
+const GameCard = ({ game }: GameProp) => {
   return (
-    <Card borderRadius={10} overflow={'hidden'}>
-        <Image src={game.background_image} />
-        <CardBody>
-            <Heading fontSize='2xl'>{game.name}</Heading>
-        </CardBody>
+    <Card borderRadius={10} overflow={"hidden"}>
+      <Image src={game.background_image} />
+      <CardBody>
+        <Heading fontSize="2xl">{game.name}</Heading>
+        <PlatformIconList
+          platforms={game.parent_platforms.map((p) => p.platform)}
+        />
+      </CardBody>
     </Card>
-  )
-}
+  );
+};
 
-export default GameCard
+export default GameCard;
