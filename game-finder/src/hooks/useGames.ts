2019b1aa72b2aa252b2a896a3db6db5e0aa9fd65
NateAra <nate.arafayne@appliedtechnology.se>
2023-09-28 22:49:07 +0200

    Added loading hook

--- game-finder/src/hooks/useGames.ts
+++ game-finder/src/hooks/useGames.ts
@@ -23,23 +23,29 @@ interface FetchGamesResponse {
 const useGames = () => {
   const [games, setGames] = useState<Game[]>([]);
   const [error, setError] = useState("");
+  const [isLoading, setLoading] = useState(false);
 
   useEffect(() => {
     const controller = new AbortController();
+    setLoading(true);
 
     axios
       .get<FetchGamesResponse>("http://localhost:3001", {
         signal: controller.signal,
       })
-      .then((response) => setGames(response.data.results))
+      .then((response) => {
+        setGames(response.data.results);
+        setLoading(false);
+      })
       .catch((error) => {
         if (error instanceof CanceledError) return;
         setError(error.message);
+        setLoading(false);
       });
     return () => controller.abort();
   }, []);
 
-  return { games, error };
+  return { games, error, isLoading };
 };
 
 export default useGames;
