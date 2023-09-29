c76e9c2e33b54935cfdf20423319cbc9c1640956
NateAra <nate.arafayne@appliedtechnology.se>
2023-09-29 22:18:35 +0200

    Rendering my GenreList

--- game-finder/src/App.tsx
+++ game-finder/src/App.tsx
@@ -1,6 +1,7 @@
 import { Grid, GridItem, Show } from "@chakra-ui/react";
 import NavBar from "./components/NavBar";
 import GameGrid from "./components/GameGrid";
+import GenreList from "./components/GenreList";
 
 const App = () => {
   return (
@@ -11,15 +12,15 @@ const App = () => {
       }}
     >
       <GridItem area={"nav"} >
-        <NavBar/>
+        <NavBar />
       </GridItem>
       <Show above="lg">
         <GridItem area={"aside"} >
-          Aside
+          <GenreList />
         </GridItem>
       </Show>
       <GridItem area={"main"} >
-        <GameGrid/>
+        <GameGrid />
       </GridItem>
     </Grid>
   );
