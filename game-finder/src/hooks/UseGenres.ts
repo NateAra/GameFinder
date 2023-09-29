23a17d964765e59150b41881ae2f40de3392cdb8
NateAra <nate.arafayne@appliedtechnology.se>
2023-09-29 22:22:16 +0200

    Made a custom hook that fetchs genres

---
+++ game-finder/src/hooks/UseGenres.ts
@@ -0,0 +1,42 @@
+import axios, { CanceledError } from "axios";
+import { useEffect, useState } from "react";
+
+interface Genre {
+  id: number;
+  name: string;
+}
+
+interface FetchGenresResponse {
+  count: number;
+  results: Genre[];
+}
+
+const useGenres = () => {
+  const [genres, setGenres] = useState<Genre[]>([]);
+  const [error, setError] = useState("");
+  const [isLoading, setLoading] = useState(false);
+
+  useEffect(() => {
+    const controller = new AbortController();
+    setLoading(true);
+
+    axios
+      .get<FetchGenresResponse>("http://localhost:3001/genres", {
+        signal: controller.signal,
+      })
+      .then((response) => {
+        setGenres(response.data.results);
+        setLoading(false);
+      })
+      .catch((error) => {
+        if (error instanceof CanceledError) return;
+        setError(error.message);
+        setLoading(false);
+      });
+    return () => controller.abort();
+  }, []);
+
+  return { genres, error, isLoading };
+};
+
+export default useGenres;
