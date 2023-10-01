667c614c5975daac5cf3a60dc57b754e3249e897
NateAra <nate.arafayne@appliedtechnology.se>
2023-10-01 13:09:44 +0200

    Refactored useGames to utilize the generic useData hook for data fetching.

--- game-finder/src/hooks/UseGenres.ts
+++ game-finder/src/hooks/UseGenres.ts
@@ -1,42 +1,10 @@
-import axios, { CanceledError } from "axios";
-import { useEffect, useState } from "react";
+import useData from "./useData";
 
-interface Genre {
+export interface Genre {
   id: number;
   name: string;
 }
 
-interface FetchGenresResponse {
-  count: number;
-  results: Genre[];
-}
-
-const useGenres = () => {
-  const [genres, setGenres] = useState<Genre[]>([]);
-  const [error, setError] = useState("");
-  const [isLoading, setLoading] = useState(false);
-
-  useEffect(() => {
-    const controller = new AbortController();
-    setLoading(true);
-
-    axios
-      .get<FetchGenresResponse>("http://localhost:3001/genres", {
-        signal: controller.signal,
-      })
-      .then((response) => {
-        setGenres(response.data.results);
-        setLoading(false);
-      })
-      .catch((error) => {
-        if (error instanceof CanceledError) return;
-        setError(error.message);
-        setLoading(false);
-      });
-    return () => controller.abort();
-  }, []);
-
-  return { genres, error, isLoading };
-};
+const useGenres = () => useData<Genre>("http://localhost:3001/genres");
 
 export default useGenres;
