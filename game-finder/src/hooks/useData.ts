775d0d823efe29a7e609250afbd8501b852e65d4
NateAra <nate.arafayne@appliedtechnology.se>
2023-10-02 22:02:16 +0200

    Refactored the code

--- game-finder/src/hooks/useData.ts
+++ game-finder/src/hooks/useData.ts
@@ -6,31 +6,38 @@ interface FetchResponse<T> {
   results: T[];
 }
 
-const useData = <T>(endpoint: string, requestConfig?: AxiosRequestConfig, deps?: any[]) => {
+const useData = <T>(
+  endpoint: string,
+  requestConfig?: AxiosRequestConfig,
+  deps?: any[]
+) => {
   const [data, setdata] = useState<T[]>([]);
   const [error, setError] = useState("");
   const [isLoading, setLoading] = useState(false);
 
-  useEffect(() => {
-    const controller = new AbortController();
-    setLoading(true);
+  useEffect(
+    () => {
+      const controller = new AbortController();
+      setLoading(true);
 
-    axios
-      .get<FetchResponse<T>>(endpoint, {
-        signal: controller.signal,
-        ...requestConfig
-      })
-      .then((response) => {
-        setdata(response.data.results);
-        setLoading(false);
-      })
-      .catch((error) => {
-        if (error instanceof CanceledError) return;
-        setError(error.message);
-        setLoading(false);
-      });
-    return () => controller.abort();
-  }, deps ? [...deps] : []);
+      axios
+        .get<FetchResponse<T>>(endpoint, {
+          signal: controller.signal,
+          ...requestConfig,
+        })
+        .then((response) => {
+          setdata(response.data.results);
+          setLoading(false);
+        })
+        .catch((error) => {
+          if (error instanceof CanceledError) return;
+          setError(error.message);
+          setLoading(false);
+        });
+      return () => controller.abort();
+    },
+    deps ? [...deps] : []
+  );
 
   return { data, error, isLoading };
 };
