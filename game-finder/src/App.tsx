d882f8c8e994cd092ccdd690e686e2a72634e31a
NateAra <nate.arafayne@appliedtechnology.se>
2023-10-02 20:34:54 +0200

    Rendering platformselector on the main area

--- game-finder/src/App.tsx
+++ game-finder/src/App.tsx
@@ -4,6 +4,7 @@ import GameGrid from "./components/GameGrid";
 import GenreList from "./components/GenreList";
 import { useState } from "react";
 import { Genre } from "./hooks/UseGenres";
+import PlatformSelector from "./components/PlatformSelector";
 
 const App = () => {
 
@@ -29,6 +30,7 @@ const App = () => {
         </GridItem>
       </Show>
       <GridItem area={"main"} >
+        <PlatformSelector />
         <GameGrid selectedGenre={selectedGenre}/>
       </GridItem>
     </Grid>
