5d09f513190579be89a6b3c3e65c1b115a7386e0
NateAra <nate.arafayne@appliedtechnology.se>
2023-10-02 22:02:39 +0200

    Refactored the code

--- game-finder/src/components/CriticScore.tsx
+++ game-finder/src/components/CriticScore.tsx
@@ -1,18 +1,17 @@
 import { Badge } from "@chakra-ui/react";
 
 interface CriteProp {
-    score: number
+  score: number;
 }
 
 const CriticScore = ({ score }: CriteProp) => {
-
-  let color = score > 75 ? 'green' : score > 60 ? 'yellow' : ''; 
+  let color = score > 75 ? "green" : score > 60 ? "yellow" : "";
 
   return (
-    <Badge colorScheme={color} fontSize="14px" paddingX={2} borderRadius='5px'>
-        {score}
+    <Badge colorScheme={color} fontSize="14px" paddingX={2} borderRadius="5px">
+      {score}
     </Badge>
-  )
-}
+  );
+};
 
-export default CriticScore
+export default CriticScore;
