5bce435b1c917842d8bef9623a0df258fcd14e27
NateAra <nate.arafayne@appliedtechnology.se>
2023-10-02 22:03:36 +0200

    Refactored the code

--- game-finder/src/components/GameCardContainer.tsx
+++ game-finder/src/components/GameCardContainer.tsx
@@ -1,16 +1,16 @@
-import { Box } from '@chakra-ui/react'
-import { ReactNode } from 'react'
+import { Box } from "@chakra-ui/react";
+import { ReactNode } from "react";
 
 interface Props {
-    children: ReactNode
+  children: ReactNode;
 }
 
 const GameCardContainer = ({ children }: Props) => {
   return (
-    <Box width='100%' borderRadius={10} overflow={"hidden"} >
-        {children}
+    <Box width="100%" borderRadius={10} overflow={"hidden"}>
+      {children}
     </Box>
-  )
-}
+  );
+};
 
-export default GameCardContainer
+export default GameCardContainer;
