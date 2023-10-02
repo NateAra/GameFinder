696f9afae130dc04cbbf0fec9f1675c0c1ce6f0b
NateAra <nate.arafayne@appliedtechnology.se>
2023-10-02 21:55:31 +0200

    Mapping platform fetch from usePlatform

--- game-finder/src/components/PlatformSelector.tsx
+++ game-finder/src/components/PlatformSelector.tsx
@@ -1,16 +1,21 @@
 import { Button, Menu, MenuButton, MenuItem, MenuList } from "@chakra-ui/react";
 import { BsChevronDown } from "react-icons/bs";
+import usePlatforms from "../hooks/usePlatforms";
 
 const PlatformSelector = () => {
+  const { data, error } = usePlatforms();
+
+  if (error) return null;
+  
   return (
     <Menu>
       <MenuButton as={Button} rightIcon={<BsChevronDown />}>
         Platform
       </MenuButton>
       <MenuList>
-        <MenuItem>Item 1</MenuItem>
-        <MenuItem>Item 2</MenuItem>
-        <MenuItem>Item 3</MenuItem>
+        {data.map((platform) => (
+          <MenuItem key={platform.id}>{platform.name}</MenuItem>
+        ))}
       </MenuList>
     </Menu>
   );
