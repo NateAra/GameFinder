81422aa2e3875bae60cb0086e41dc2d988d96ebd
NateAra <nate.arafayne@appliedtechnology.se>
2023-10-04 21:47:32 +0200

    Added useState for state management

--- game-finder/src/App.tsx
+++ game-finder/src/App.tsx
@@ -5,9 +5,12 @@ import GenreList from "./components/GenreList";
 import { useState } from "react";
 import { Genre } from "./hooks/UseGenres";
 import PlatformSelector from "./components/PlatformSelector";
+import { Platform } from "./hooks/useGames";
 
 const App = () => {
   const [selectedGenre, setSelectedGenre] = useState<Genre | null>(null);
+
+  const [selectedPlatform, setSelectedPlatform] = useState<Platform | null>(null);
 
   return (
     <Grid
