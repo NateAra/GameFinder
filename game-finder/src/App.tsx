591547bf4e58aa696df27293ce42caeab8444827
NateAra <nate.arafayne@appliedtechnology.se>
2023-10-05 18:32:29 +0200

    Rendering sort selector

--- game-finder/src/App.tsx
+++ game-finder/src/App.tsx
@@ -1,4 +1,4 @@
-import { Grid, GridItem, Show } from "@chakra-ui/react";
+import { Grid, GridItem, HStack, Show } from "@chakra-ui/react";
 import NavBar from "./components/NavBar";
 import GameGrid from "./components/GameGrid";
 import GenreList from "./components/GenreList";
@@ -6,6 +6,7 @@ import { useState } from "react";
 import { Genre } from "./hooks/UseGenres";
 import PlatformSelector from "./components/PlatformSelector";
 import { Platform } from "./hooks/useGames";
+import SortSelector from "./components/SortSelector";
 
 interface GameQuery {
   genre: Genre | null;
@@ -13,7 +14,7 @@ interface GameQuery {
 }
 
 const App = () => {
-  const [gameQuery, setGameQuery] = useState<GameQuery>({} as GameQuery); 
+  const [gameQuery, setGameQuery] = useState<GameQuery>({} as GameQuery);
 
   return (
     <Grid
@@ -38,8 +39,19 @@ const App = () => {
         </GridItem>
       </Show>
       <GridItem area={"main"}>
-        <PlatformSelector selectedPlatform={gameQuery.platform} onSelectPlatform={(platform) =>  setGameQuery({ ...gameQuery, platform })} />
-        <GameGrid selectedPlatform={gameQuery.platform} selectedGenre={gameQuery.genre} />
+        <HStack spacing={5} paddingLeft={37}>
+          <PlatformSelector
+            selectedPlatform={gameQuery.platform}
+            onSelectPlatform={(platform) =>
+              setGameQuery({ ...gameQuery, platform })
+            }
+          />
+          <SortSelector />
+        </HStack>
+        <GameGrid
+          selectedPlatform={gameQuery.platform}
+          selectedGenre={gameQuery.genre}
+        />
       </GridItem>
     </Grid>
   );
