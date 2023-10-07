88f6c4d967386095b9540f8b2c0680c245e58ab9
NateAra <nate.arafayne@appliedtechnology.se>
2023-10-07 14:19:36 +0200

    Rendering search input

--- game-finder/src/components/NavBar.tsx
+++ game-finder/src/components/NavBar.tsx
@@ -1,11 +1,13 @@
 import { HStack, Image } from "@chakra-ui/react";
 import Logo from "../assets/Logo.png";
 import ColorModeSwitch from "./ColorModeSwitch";
+import SearchInput from "./SearchInput";
 
 const NavBar = () => {
   return (
-    <HStack justifyContent={"space-between"} padding={"10px"}>
+    <HStack padding={"10px"}>
       <Image src={Logo} boxSize={"50px"} borderRadius={"1g"} />
+      <SearchInput/>
       <ColorModeSwitch />
     </HStack>
   );
